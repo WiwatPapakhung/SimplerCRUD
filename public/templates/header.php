26d64de67cbd30189d7b3111ecb2ac6f134e31b8
Wiwat Papakhung <wiwat18289@hotmail.com>
2019-10-05 22:38:13 +0700

    update

--- public/templates/header.php
+++ public/templates/header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="x-ua-compatible" content="ie=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-    <title>Simple Database App</title>
+    <title>Simple Database App Wiwat Papakhung UTCC 1830731303003</title>
 
     <link rel="stylesheet" href="css/style.css" />
   </head>
